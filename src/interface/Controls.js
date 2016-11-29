0d440b4d2ce0fa56ecc28ac1a38f424a34e1d358
Will Busby <will@bmas.agency>
2016-11-29 22:37:58 +0000

    wsad keys move, not arrows (but should change that to arrows soon)

--- src/interface/Controls.js
+++ src/interface/Controls.js
@@ -8,7 +8,7 @@ class Controls extends SuperComponent {
 		return (
 			<div>
 				<h2>Controls</h2>
-				<p>Use the arrow keys to move</p>
+				<p>Use w s a d keys to move</p>
 				<p>Press F to hit</p>
 				<p>Press G to transform</p>
 				<span className="btn" onClick={ this.props.setView.bind(this, 'main') }>Main Menu</span>
