dc5422e596c0b828ffc5d75f872ac018b4f6d57c
Will Busby <Will Busby>
2017-01-10 22:49:08 +0000

    added procfile and server db stuff

--- server/db/Connection.js
+++ server/db/Connection.js
@@ -19,9 +19,10 @@
 	}
 
 	function getConnection() {
-		const DBNAME = 'mongodb://localhost/supersand';
+		let env = process.env.NODE_ENV || 'dev';
+		const DBNAME = env === 'production' ? 'ds161518.mlab.com:61518/heroku_7k27vqpx' : 'mongodb://localhost/supersand';
 		const USER = 'admin';
-		const PASSWORD = 'admin';
+		const PASSWORD = env === 'production' ? 'busbybmw69' : 'admin';
 
 
 		var connection = new Connection(DBNAME, USER, PASSWORD);
@@ -32,6 +33,6 @@
 		if (!conn) {
 			conn = getConnection();
 		}
-		return conn; 
+		return conn;
 	}());
 }());
